d45f59dd4226180fc54764879f0591a92391b1af
Nishant Arun <nishantarun2512@gmail.com>
2025-12-28 14:42:43 +0530

    Server bootstrap working – app/server separation fixed

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./app.js";
+import mongoose from "mongoose";
+
+import chalk from "chalk";
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(chalk.green("MongoDB Connected"));
+
+    app.listen(PORT, () => {
+      console.log("Server Hosted: " + chalk.cyan(`http://localhost:${PORT}`));
+    });
+  } catch (error) {
+    console.error(chalk.red("Failed to start server"), error);
+    process.exit(1);
+  }
+};
+
+startServer();
